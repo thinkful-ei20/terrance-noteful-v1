02775c50dbfb9c9ad6c4beb125ed2ced7e94670e
Terrance Corley <terrancecorley@outlook.com>
2018-04-16 14:29:27 -0700

    completed assignment

--- server.js
+++ server.js
@@ -5,3 +5,29 @@ const data = require('./db/notes');
 console.log('Hello Noteful!');
 
 // INSERT EXPRESS APP CODE HERE...
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/api/notes', (req, res) => {
+  if (req.query.searchTerm) {
+    let query = req.query.searchTerm;
+    let match = data.filter(item => item.title.includes(query));
+    res.json(match);
+  } else {
+    res.json(data);
+  }
+});
+
+app.get('/api/notes/:id', (req, res) => {
+  const {id} = req.params;
+  let match = data.find(item => item.id === Number(id))
+  res.json(match);
+});
+
+app.listen(8080, function () {
+  console.info(`Server listening on ${this.address().port}`);
+}).on('error', err => {
+  console.error(err);
+});
