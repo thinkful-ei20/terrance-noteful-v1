ee281dfdb491d24f0b308058063162ee69ab50f5
Terrance Corley <terrancecorley@outlook.com>
2018-04-18 20:58:06 -0700

    refactored router delete functionality

--- router/notes.router.js
+++ router/notes.router.js
@@ -89,13 +89,11 @@ router.post('/notes', (req, res, next) => {
 router.delete('/notes/:id', (req, res, next) => {
   let {id} = req.params;
 
-  notes.delete(id, err => {
+  notes.delete(id, (err) => {
     if (err) {
       return next(err);
     } 
-    if (err === null) {
-      res.send(204).end();
-    }
+    res.sendStatus(204);
   });
 });
 
