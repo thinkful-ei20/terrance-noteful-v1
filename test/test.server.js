348cd9f68cfb5a8a76e73b1010e5fc6792bb41a5
Terrance Corley <terrancecorley@outlook.com>
2018-04-20 14:50:26 -0700

    added testing for notes/:id

--- test/test.server.js
+++ test/test.server.js
@@ -46,7 +46,7 @@ describe('404 handler', function() {
 
 });
 
-describe('GET default notes', function() {
+describe('GET /api/notes', function() {
   
   it('should return the default of 10 notes as an array', function() {
     return chai.request(app)
@@ -65,5 +65,41 @@ describe('GET default notes', function() {
         });
       });
   });
+
+  it('should return correct search results for a valid query', function() {
+    return chai.request(app)
+      .get('/api/notes/?searchTerm=cats')
+      .then( function(res) {
+        expect(res).to.have.status(200);
+      });
+  });
+
+  it('should return an empty array for an incorrect query', function() {
+    return chai.request(app)
+      .get('/api/notes/?searchTerm=12345DoesNoteExistBruh')
+      .then( function(res) {
+        expect(res.body).to.be.a('array');
+        expect(res.body).to.have.lengthOf(0);
+      });
+  });
+
+});
+
+describe('GET /api/notes/:id', function() {
+
+  it('should return correct note object with id, title and content for a given id', function() {
+    return chai.request(app)
+      .get('/api/notes/1001')
+      .then( function(res) {
+        expect(res.body).to.have.all.keys('id', 'title', 'content');
+        expect(res.body.id).to.equal(1001);
+        expect(res).status(200);
+      });
+  });
+
+  // it('should respond with a 404 for an invalid id', function() {
+  //   return chai.request(app)
+  //     .get('/api/notes/DOESNOTEXIT')
+  // });
 
 });
